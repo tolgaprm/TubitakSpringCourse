7c58b06cf2ddc2729edef5b1d4ea74d47afe8da8
Tolga Pirim <tolgapirim25@gmail.com>
2023-10-09 14:53:54 +0300

    Using @PreAuthorize

--- spring-security/src/main/java/yte/intern/springsecurity/configuration/SecurityConfiguration.java
+++ spring-security/src/main/java/yte/intern/springsecurity/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package yte.intern.springsecurity.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
@@ -41,8 +43,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
-                .requestMatchers(AntPathRequestMatcher.antMatcher("/user")).hasAnyRole("USER", "ADMIN")
-                .requestMatchers(AntPathRequestMatcher.antMatcher("/admin")).hasRole("ADMIN")
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .and()
--- spring-security/src/main/java/yte/intern/springsecurity/uygulama/UserController.java
+++ spring-security/src/main/java/yte/intern/springsecurity/uygulama/UserController.java
@@ -1,5 +1,6 @@
 package yte.intern.springsecurity.uygulama;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public String user() {
         return "User Endpoint";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String admin() {
         return "Admin Endpoint";
