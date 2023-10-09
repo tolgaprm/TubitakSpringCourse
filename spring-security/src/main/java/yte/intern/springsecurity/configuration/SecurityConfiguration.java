f494b62abbb3a91c826f332b382b1649259b36ed
Tolga Pirim <tolgapirim25@gmail.com>
2023-10-09 15:03:57 +0300

    Write CustomAuth

--- spring-security/src/main/java/yte/intern/springsecurity/configuration/SecurityConfiguration.java
+++ spring-security/src/main/java/yte/intern/springsecurity/configuration/SecurityConfiguration.java
@@ -5,38 +5,37 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import yte.intern.springsecurity.customAuth.CustomDetailsService;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfiguration {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final CustomDetailsService customDetailsService;
 
-    public SecurityConfiguration(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    public SecurityConfiguration(AuthenticationManagerBuilder authenticationManagerBuilder, CustomDetailsService customDetailsService) throws Exception {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.customDetailsService = customDetailsService;
         createUser();
     }
 
     private void createUser() throws Exception {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("user")
-                .authorities("ROLE_USER")
-                .build();
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password("user")
+//                .authorities("ROLE_USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password("admin")
+//                .authorities("ROLE_ADMIN")
+//                .build();
 
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("admin")
-                .authorities("ROLE_ADMIN")
-                .build();
-
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .withUser(user)
-                .withUser(admin)
+        authenticationManagerBuilder
+                .userDetailsService(customDetailsService)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
